b3d82df6e5163439fad1bb5d258bc8576828246c
Andrew Nesbitt <andrewnez@gmail.com>
2025-02-21 17:24:02 +0000

    Add bootstrap-icons-helper gem and update application styles and meta information

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def meta_title
+    [@meta_title, 'Ecosyste.ms: Resolve'].compact.join(' | ')
+  end
+
+  def meta_description
+    @meta_description || app_description
+  end
+
+  def app_name
+    "Resolve"
+  end
+
+  def app_description
+    'An open API service to resolve dependency trees of packages for many open source software ecosystems. '
+  end
 end
