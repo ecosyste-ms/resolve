736ab5b41051c2be0651f643c39eb6b41188f203
Andrew Nesbitt <andrewnez@gmail.com>
2026-03-27 16:21:08 +0000

    Fix HTML resolve page creating duplicate jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,15 @@
 class JobsController < ApplicationController
   def resolve
     raise ActionController::RoutingError.new('Not Found') if (params[:registry].blank? && params[:ecosystem].blank?) || params[:package_name].blank?
-    @job = Job.find_by(registry: params[:registry], package_name: params[:package_name], before: params[:before], version: params[:version])
+    @job = Job.where(registry: params[:registry], package_name: params[:package_name], before: params[:before], version: version)
+                .where.not(status: 'error')
+                .order(created_at: :desc)
+                .first
     if @job.nil?
       @job = Job.create(registry: params[:registry], ecosystem: params[:ecosystem], package_name: params[:package_name], status: 'pending', ip: request.remote_ip, before: params[:before], version: version, tree: params[:tree] == 'true')
       @job.resolve_async
     end
+    @job.check_status if @job.status.in?(%w[queued working])
   end
 
   def version
